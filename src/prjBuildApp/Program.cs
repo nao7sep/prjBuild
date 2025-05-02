77a3323527f64ba574cd0619d9f398ca70104562
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2025-05-02 14:30:52 +0900

    Add initial project files including .gitignore, solution, project files, and sample Program

---
+++ src/prjBuildApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prjBuildApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
